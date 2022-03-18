c359d4bf59b9fff4616e6e172061690a426501d3
JacobMarsay <jmmarsay@outlook.com>
2022-03-18 14:25:23 +0000

    Login page designed for desktop

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -49,122 +49,129 @@
 <!DOCTYPE html>
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <body>
-    <div class="background_container"> 
-        <div class="background">
-            <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 1024">
-                <g clip-path="url(#a)">
-                  <path fill="#0792A5" d="M0 0h1440v1024H0z"/>
-                  <path d="m26.716 234.178 173.315-93.594 10.684-12.861 8.919-16.924.068-26.255-6.601-21.499-12.838-12.206-173.304 89.298-.243 94.041Z" fill="#FAFAFA"/>
-                  <path d="m26.716 234.178 173.315-93.594 10.684-12.861 8.919-16.924.068-26.255-6.601-21.499-12.838-12.206-173.304 89.298-.243 94.041Z" fill="url(#b)"/>
-                  <path d="M113.429-30.42-60.513 62.783l-10.741 12.848-8.984 16.921-.137 26.295 6.558 21.551 12.834 12.263 173.92-88.9.492-94.181Z" fill="#FAFAFA"/>
-                  <path d="M113.429-30.42-60.513 62.783l-10.741 12.848-8.984 16.921-.137 26.295 6.558 21.551 12.834 12.263 173.92-88.9.492-94.181Z" fill="url(#c)"/>
-                  <g filter="url(#d)">
-                    <path d="m200.641 17.848-174.03-94.5-.243 94.05c86.425 52.458 205.647 94.586 173.956 123.218 82.283-50.826 41.224-100.468.317-122.768Z" fill="#E5E5E5"/>
-                  </g>
-                  <g filter="url(#e)">
-                    <path d="m-61.154 185.41 173.626 94.923.491-93.942C26.753 133.729-92.251 91.285-60.513 62.783c-82.344 50.517-41.453 100.228-.641 122.627Z" fill="#E5E5E5"/>
-                  </g>
-                  <path d="m1484.64 980.839-124.37-206.791-15.89-12.139-20.44-9.605-30.46 2.167-24.34 10.024-12.99 16.958 119.38 207.146 109.11-7.76Z" fill="#FAFAFA"/>
-                  <path d="m1484.64 980.839-124.37-206.791-15.89-12.139-20.44-9.605-30.46 2.167-24.34 10.024-12.99 16.958 119.38 207.146 109.11-7.76Z" fill="url(#f)"/>
-                  <path d="m1169.81 896.044 123.97 207.606 15.88 12.21 20.45 9.68 30.51-2.08 24.4-9.98 13.06-16.96-118.98-207.939-109.29 7.463Z" fill="#FAFAFA"/>
-                  <path d="m1169.81 896.044 123.97 207.606 15.88 12.21 20.45 9.68 30.51-2.08 24.4-9.98 13.06-16.96-118.98-207.939-109.29 7.463Z" fill="url(#g)"/>
-                  <g filter="url(#h)">
-                    <path d="m1217.85 783.812-93.75 223.798 109.12-7.757c52.97-111.616 90.97-262.995 127.06-226.17-66.45-97.629-120.29-42.484-142.43 10.129Z" fill="#E5E5E5"/>
-                  </g>
-                  <g filter="url(#i)">
-                    <path d="m1436.08 1093.93 94.28-223.335-109.01 7.443c-53.23 111.366-91.61 262.502-127.57 225.612 66.1 97.73 120.04 42.78 142.3-9.72Z" fill="#E5E5E5"/>
-                  </g>
-                </g>
-                <defs>
-                  <linearGradient id="b" x1="219.552" y1="142.783" x2="26.954" y2="142.285" gradientUnits="userSpaceOnUse">
-                    <stop offset=".147" stop-color="#0792A5" stop-opacity=".11"/>
-                    <stop offset=".998" stop-color="#0792A5"/>
-                    <stop offset="1" stop-color="#0792A5"/>
-                  </linearGradient>
-                  <linearGradient id="c" x1="-80.07" y1="60.521" x2="112.949" y2="61.53" gradientUnits="userSpaceOnUse">
-                    <stop offset=".147" stop-color="#0792A5" stop-opacity=".11"/>
-                    <stop offset=".998" stop-color="#0792A5"/>
-                    <stop offset="1" stop-color="#0792A5"/>
-                  </linearGradient>
-                  <linearGradient id="f" x1="1361.05" y1="749.665" x2="1378.03" y2="988.422" gradientUnits="userSpaceOnUse">
-                    <stop offset=".147" stop-color="#0792A5" stop-opacity=".11"/>
-                    <stop offset=".998" stop-color="#0792A5"/>
-                    <stop offset="1" stop-color="#0792A5"/>
-                  </linearGradient>
-                  <linearGradient id="g" x1="1292.94" y1="1128.08" x2="1276.6" y2="888.752" gradientUnits="userSpaceOnUse">
-                    <stop offset=".147" stop-color="#0792A5" stop-opacity=".11"/>
-                    <stop offset=".998" stop-color="#0792A5"/>
-                    <stop offset="1" stop-color="#0792A5"/>
-                  </linearGradient>
-                  <filter id="d" x="22.368" y="-76.652" width="229.512" height="231.268" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-                    <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-                    <feOffset dy="10"/>
-                    <feGaussianBlur stdDeviation="2"/>
-                    <feComposite in2="hardAlpha" operator="out"/>
-                    <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-                    <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_305_2"/>
-                    <feBlend in="SourceGraphic" in2="effect1_dropShadow_305_2" result="shape"/>
-                  </filter>
-                  <filter id="e" x="-112.19" y="62.783" width="229.153" height="231.55" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-                    <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-                    <feOffset dy="10"/>
-                    <feGaussianBlur stdDeviation="2"/>
-                    <feComposite in2="hardAlpha" operator="out"/>
-                    <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-                    <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_305_2"/>
-                    <feBlend in="SourceGraphic" in2="effect1_dropShadow_305_2" result="shape"/>
-                  </filter>
-                  <filter id="h" x="1120.1" y="719.92" width="244.183" height="301.693" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-                    <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-                    <feOffset dy="10"/>
-                    <feGaussianBlur stdDeviation="2"/>
-                    <feComposite in2="hardAlpha" operator="out"/>
-                    <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-                    <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_305_2"/>
-                    <feBlend in="SourceGraphic" in2="effect1_dropShadow_305_2" result="shape"/>
-                  </filter>
-                  <filter id="i" x="1289.78" y="870.595" width="244.576" height="300.974" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-                    <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-                    <feOffset dy="10"/>
-                    <feGaussianBlur stdDeviation="2"/>
-                    <feComposite in2="hardAlpha" operator="out"/>
-                    <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-                    <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_305_2"/>
-                    <feBlend in="SourceGraphic" in2="effect1_dropShadow_305_2" result="shape"/>
-                  </filter>
-                  <clipPath id="a">
-                    <path fill="#fff" d="M0 0h1440v1024H0z"/>
-                  </clipPath>
-                </defs>
-              </svg>
-              
-            <div class="form_container">
-                <h1 class="title">Login TITLE</h1>
-                <form class="login_form" action="{{ route('authenticate') }}" method="POST">
-                    @csrf
-                    <div class="">
-                        <label for="email">Email:</label>
-                        <input type="email" id="email" name="email">
-                        @error('email')
-                        <div class="alert alert-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-
-                    <div class="">
-                        <label for="password">Password:</label>
-                        <input type="password" id="password" name="password">
-                        @error('password')
-                        <div class="alert alert-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <button type="submit" class="btn btn-blue">Login</button>
-                </form>
-                <p>Don't have an account? <a href="users/registration">SIGN UP</a></p>
-            </div>
-        </div>
-    </div> 
+  {{-- <div class="background_container">  --}}
+  <div class="background">
+    <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1420 1024">
+      <g clip-path="url(#a)">
+        <path fill="#0792A5" d="M0 0h1440v1024H0z"/>
+        <path d="m26.716 234.178 173.315-93.594 10.684-12.861 8.919-16.924.068-26.255-6.601-21.499-12.838-12.206-173.304 89.298-.243 94.041Z" fill="#FAFAFA"/>
+        <path d="m26.716 234.178 173.315-93.594 10.684-12.861 8.919-16.924.068-26.255-6.601-21.499-12.838-12.206-173.304 89.298-.243 94.041Z" fill="url(#b)"/>
+        <path d="M113.429-30.42-60.513 62.783l-10.741 12.848-8.984 16.921-.137 26.295 6.558 21.551 12.834 12.263 173.92-88.9.492-94.181Z" fill="#FAFAFA"/>
+        <path d="M113.429-30.42-60.513 62.783l-10.741 12.848-8.984 16.921-.137 26.295 6.558 21.551 12.834 12.263 173.92-88.9.492-94.181Z" fill="url(#c)"/>
+        <g filter="url(#d)">
+          <path d="m200.641 17.848-174.03-94.5-.243 94.05c86.425 52.458 205.647 94.586 173.956 123.218 82.283-50.826 41.224-100.468.317-122.768Z" fill="#E5E5E5"/>
+        </g>
+        <g filter="url(#e)">
+          <path d="m-61.154 185.41 173.626 94.923.491-93.942C26.753 133.729-92.251 91.285-60.513 62.783c-82.344 50.517-41.453 100.228-.641 122.627Z" fill="#E5E5E5"/>
+        </g>
+        <path d="m1484.64 980.839-124.37-206.791-15.89-12.139-20.44-9.605-30.46 2.167-24.34 10.024-12.99 16.958 119.38 207.146 109.11-7.76Z" fill="#FAFAFA"/>
+        <path d="m1484.64 980.839-124.37-206.791-15.89-12.139-20.44-9.605-30.46 2.167-24.34 10.024-12.99 16.958 119.38 207.146 109.11-7.76Z" fill="url(#f)"/>
+        <path d="m1169.81 896.044 123.97 207.606 15.88 12.21 20.45 9.68 30.51-2.08 24.4-9.98 13.06-16.96-118.98-207.939-109.29 7.463Z" fill="#FAFAFA"/>
+        <path d="m1169.81 896.044 123.97 207.606 15.88 12.21 20.45 9.68 30.51-2.08 24.4-9.98 13.06-16.96-118.98-207.939-109.29 7.463Z" fill="url(#g)"/>
+        <g filter="url(#h)">
+          <path d="m1217.85 783.812-93.75 223.798 109.12-7.757c52.97-111.616 90.97-262.995 127.06-226.17-66.45-97.629-120.29-42.484-142.43 10.129Z" fill="#E5E5E5"/>
+        </g>
+        <g filter="url(#i)">
+          <path d="m1436.08 1093.93 94.28-223.335-109.01 7.443c-53.23 111.366-91.61 262.502-127.57 225.612 66.1 97.73 120.04 42.78 142.3-9.72Z" fill="#E5E5E5"/>
+        </g>
+      </g>
+      <defs>
+        <linearGradient id="b" x1="219.552" y1="142.783" x2="26.954" y2="142.285" gradientUnits="userSpaceOnUse">
+          <stop offset=".147" stop-color="#0792A5" stop-opacity=".11"/>
+          <stop offset=".998" stop-color="#0792A5"/>
+          <stop offset="1" stop-color="#0792A5"/>
+        </linearGradient>
+        <linearGradient id="c" x1="-80.07" y1="60.521" x2="112.949" y2="61.53" gradientUnits="userSpaceOnUse">
+          <stop offset=".147" stop-color="#0792A5" stop-opacity=".11"/>
+          <stop offset=".998" stop-color="#0792A5"/>
+          <stop offset="1" stop-color="#0792A5"/>
+        </linearGradient>
+        <linearGradient id="f" x1="1361.05" y1="749.665" x2="1378.03" y2="988.422" gradientUnits="userSpaceOnUse">
+          <stop offset=".147" stop-color="#0792A5" stop-opacity=".11"/>
+          <stop offset=".998" stop-color="#0792A5"/>
+          <stop offset="1" stop-color="#0792A5"/>
+        </linearGradient>
+        <linearGradient id="g" x1="1292.94" y1="1128.08" x2="1276.6" y2="888.752" gradientUnits="userSpaceOnUse">
+          <stop offset=".147" stop-color="#0792A5" stop-opacity=".11"/>
+          <stop offset=".998" stop-color="#0792A5"/>
+          <stop offset="1" stop-color="#0792A5"/>
+        </linearGradient>
+        <filter id="d" x="22.368" y="-76.652" width="229.512" height="231.268" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+          <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+          <feOffset dy="10"/>
+          <feGaussianBlur stdDeviation="2"/>
+          <feComposite in2="hardAlpha" operator="out"/>
+          <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+          <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_305_2"/>
+          <feBlend in="SourceGraphic" in2="effect1_dropShadow_305_2" result="shape"/>
+        </filter>
+        <filter id="e" x="-112.19" y="62.783" width="229.153" height="231.55" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+          <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+          <feOffset dy="10"/>
+          <feGaussianBlur stdDeviation="2"/>
+          <feComposite in2="hardAlpha" operator="out"/>
+          <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+          <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_305_2"/>
+          <feBlend in="SourceGraphic" in2="effect1_dropShadow_305_2" result="shape"/>
+        </filter>
+        <filter id="h" x="1120.1" y="719.92" width="244.183" height="301.693" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+          <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+          <feOffset dy="10"/>
+          <feGaussianBlur stdDeviation="2"/>
+          <feComposite in2="hardAlpha" operator="out"/>
+          <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+          <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_305_2"/>
+          <feBlend in="SourceGraphic" in2="effect1_dropShadow_305_2" result="shape"/>
+        </filter>
+        <filter id="i" x="1289.78" y="870.595" width="244.576" height="300.974" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+          <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+          <feOffset dy="10"/>
+          <feGaussianBlur stdDeviation="2"/>
+          <feComposite in2="hardAlpha" operator="out"/>
+          <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+          <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_305_2"/>
+          <feBlend in="SourceGraphic" in2="effect1_dropShadow_305_2" result="shape"/>
+        </filter>
+        <clipPath id="a">
+          <path fill="#fff" d="M0 0h1440v1024H0z"/>
+        </clipPath>
+      </defs>
+    </svg>
+    <div class="form_container">
+      <h1 class="title">Login</h1>
+      <p>Please sign in to continue</p>
+      <form class="login_form" action="{{ route('authenticate') }}" method="POST">
+          @csrf
+          <div class="login_input">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" xml:space="preserve">
+              <path d="M3.217 126.14 224.87 302.686c1.579 1.331 15.821 13.047 31.13 13.047 15.377 0 29.747-11.281 31.292-12.535l221.5-177.067a8.519 8.519 0 0 0 3.208-6.664C512 95.94 492.86 76.8 469.333 76.8H42.667C19.14 76.8 0 95.94 0 119.467a8.535 8.535 0 0 0 3.217 6.673zm39.45-32.273h426.667c12.8 0 23.441 9.446 25.31 21.726L276.599 289.894c-3.038 2.44-12.672 8.772-20.599 8.772-7.689 0-17.109-6.502-20.284-9.165L17.357 115.584c1.877-12.279 12.51-21.717 25.31-21.717zM156.86 274.893 37.393 368.759c-3.703 2.91-4.343 8.277-1.434 11.981a8.502 8.502 0 0 0 6.707 3.26 8.498 8.498 0 0 0 5.274-1.826l119.467-93.867c3.703-2.91 4.344-8.277 1.434-11.981-2.91-3.712-8.278-4.343-11.981-1.433zM355.14 274.893c-3.703-2.91-9.071-2.27-11.981 1.434s-2.27 9.071 1.434 11.981l119.467 93.867a8.492 8.492 0 0 0 5.265 1.826 8.52 8.52 0 0 0 6.716-3.26c2.91-3.703 2.27-9.071-1.434-11.981L355.14 274.893z"/>
+              <path d="M503.467 153.6a8.536 8.536 0 0 0-8.533 8.533v230.4c0 14.114-11.486 25.6-25.6 25.6H42.667c-14.114 0-25.6-11.486-25.6-25.6v-230.4c0-4.71-3.823-8.533-8.533-8.533S0 157.423 0 162.133v230.4C0 416.06 19.14 435.2 42.667 435.2h426.667c23.526 0 42.667-19.14 42.667-42.667v-230.4c-.001-4.71-3.824-8.533-8.534-8.533z"/>
+            </svg>
+              <input type="email" id="email" name="email" placeholder="Email">
+              {{-- @error('email')
+              <div class="alert alert-danger">{{ $message }}</div>
+              @enderror --}}
+          </div>
+          <div class="login_input">
+            
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0.47475242614746094 0.00007295608520507812 52.78624725341797 53.734928131103516">
+              <path d="M52.951 43.012 32.108 23.145c3.086-6.662 2.125-13.407-2.632-18.164C26.122 1.627 21.473-.197 16.738.017c-4.808.201-9.176 2.35-12.3 6.047-5.673 6.718-5.203 16.835 1.071 23.032 4.379 4.326 10.551 5.762 16.616 3.897l2.395 2.442c.188.191.445.3.714.3h4.027v3a1 1 0 0 0 1 1h3v3a1 1 0 0 0 1 1h4v4a1 1 0 0 0 1 1h2v1.776c0 .965.083 1.503.843 2.263.556.558 1.295.897 2.157.961h8a1 1 0 0 0 1-1v-9a1 1 0 0 0-.31-.723zm-1.69 8.724-6.923.003a1.292 1.292 0 0 1-.819-.377c-.258-.258-.258-.258-.258-.85v-2.776a1 1 0 0 0-1-1h-2v-4a1 1 0 0 0-1-1h-4v-3a1 1 0 0 0-1-1h-3v-3a1 1 0 0 0-1-1h-4.607l-2.534-2.585a.999.999 0 0 0-1.039-.245c-5.525 1.9-11.194.692-15.166-3.231C1.379 22.204.962 13.28 5.966 7.355c2.759-3.266 6.614-5.162 10.855-5.34a15.054 15.054 0 0 1 11.24 4.38c4.339 4.34 5.045 10.361 1.936 16.521a.999.999 0 0 0 .203 1.174l21.06 20.075v7.571z"></path>
+              <path d="M8.97 8.516c-1.133 1.134-1.757 2.641-1.757 4.243s.624 3.109 1.757 4.242c1.133 1.134 2.64 1.758 4.243 1.758s3.109-.624 4.243-1.758c1.133-1.133 1.757-2.64 1.757-4.242s-.624-3.109-1.757-4.243a6.007 6.007 0 0 0-8.486 0zm7.071 7.071c-1.512 1.512-4.146 1.512-5.657 0-.755-.756-1.171-1.76-1.171-2.828s.416-2.073 1.171-2.829c.78-.779 1.804-1.169 2.829-1.169a3.99 3.99 0 0 1 4 3.998 3.973 3.973 0 0 1-1.172 2.828z"></path>
+            </svg>
+            <input type="password" id="password" name="password" placeholder="Password">
+              {{-- @error('password')
+              <div class="alert alert-danger">{{ $message }}</div>
+              @enderror --}}
+          </div>
+          <button type="submit" class="login_button">Login</button>
+      </form>
+      <p>Don't have an account? <a href="users/registration-index">SIGN UP</a></p>
+    </div>
+  </div>
+  {{-- </div> --}}
+  
 </body>
